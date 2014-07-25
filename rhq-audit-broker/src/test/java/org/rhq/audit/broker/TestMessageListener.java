1b23b5bc6ff2a9748ad6046d4b3f54229340ddd4
John Mazzitelli <mazz@redhat.com>
2014-07-25 14:32:35 -0400

    got the embedded broker working, with configuration files and unit tests showing messages flowing

---
+++ rhq-audit-broker/src/test/java/org/rhq/audit/broker/TestMessageListener.java
@@ -0,0 +1,37 @@
+package org.rhq.audit.broker;
+
+import java.util.ArrayList;
+import java.util.concurrent.CountDownLatch;
+
+import javax.jms.Message;
+import javax.jms.MessageListener;
+import javax.jms.TextMessage;
+
+/**
+ * Creates a simple test listener. Logs messages and errors it gets in lists
+ * given to it. This listener will notify when it gets a message by counting
+ * down a latch.
+ */
+public class TestMessageListener implements MessageListener {
+
+    private final CountDownLatch latch;
+    private final ArrayList<String> messages;
+    private final ArrayList<String> errors;
+
+    public TestMessageListener(CountDownLatch latch, ArrayList<String> messages, ArrayList<String> errors) {
+        this.latch = latch;
+        this.messages = messages;
+        this.errors = errors;
+    }
+
+    public void onMessage(Message message) {
+        try {
+            String receivedMessage = ((TextMessage) message).getText();
+            messages.add(receivedMessage);
+        } catch (Exception e) {
+            errors.add(e.toString());
+        } finally {
+            latch.countDown();
+        }
+    }
+}
